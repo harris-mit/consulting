73b62e84844e3c8c071b274e0f88982e5b80663c
Mitchell Harris <mitchell.harris@mit.edu>
2025-01-13 21:37:53 -0500

    changed relative directories

--- src/components/ConsultingWebsite.jsx
+++ src/components/ConsultingWebsite.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '../../components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '../components/ui/dialog';
 import { Input } from '../components/ui/input';
 import { Textarea } from '../components/ui/textarea';
 
