e8b9f2bd8b8096ad63f3fdfffcf0e1cd9a4b03c5
Mitchell Harris <mitchell.harris@mit.edu>
2025-01-13 20:21:01 -0500

    updating for netlify

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  build: {
+    outDir: 'dist'
+  }
 })
