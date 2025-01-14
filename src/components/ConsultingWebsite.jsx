480544ec9d0ede67170403e719567d271b40809c
Mitchell Harris <mitchell.harris@mit.edu>
2025-01-13 21:24:28 -0500

    edited ui component location

--- src/components/ConsultingWebsite.jsx
+++ src/components/ConsultingWebsite.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '../../components/ui/card';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '../../components/ui/dialog';
 import { Input } from '../../components/ui/input';
 import { Textarea } from '../../components/ui/textarea';
